e20877eaf2e786784dac14f822b9804570b612ca
Michael Twomey <micktwomey@gmail.com>
2014-02-17 00:48:37 +0000

    Gave up trying to use carousel, much quicker and more effective to start from scratch.
    Also experimenting with different colour themes.

--- go.py
+++ go.py
@@ -7,13 +7,17 @@ def main():
     prefix = os.path.abspath(os.path.dirname(__file__))
     virtualenv = os.path.join(prefix, "gamecraft-it-py")
     python = os.path.join(virtualenv, "bin", "python")
+    pip = os.path.join(virtualenv, "bin", "pip")
+    requirements = os.path.join(prefix, "requirements.txt")
     if not os.path.isfile(python):
         print("Attempting to bootstrap, hold on to your pants!")
         subprocess.check_call(["virtualenv", virtualenv])
-        pip = os.path.join(virtualenv, "bin", "pip")
-        requirements = os.path.join(prefix, "requirements.txt")
         subprocess.check_call([pip, "install", "-r", requirements])
         subprocess.check_call([pip, "install", "-e", prefix])
+    if sys.argv[-1] == "update":
+        subprocess.check_call([pip, "install", "-U", "-r", requirements])
+        print("All updated, please run python go.py again.")
+        sys.exit(0)
 
     subprocess.call([python, "-m", "gamecraft.main"] + sys.argv[1:])
 
